4da8ac943310d4aebbc890556e6079bcd3382810
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-01-23 08:51:09 -0300

    Implementação da Validação do Formulário do Component Chamado-Create

--- HelpDesk/frontend/src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ HelpDesk/frontend/src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ChamadoService } from 'src/app/services/chamado.service';
 
 @Component({
   selector: 'app-chamado-create',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class ChamadoCreateComponent {
 
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status: FormControl = new FormControl(null, [Validators.required]);
+  titulo: FormControl = new FormControl(null, [Validators.required]);
+  descricao: FormControl = new FormControl(null, [Validators.required]);
+  tecnico: FormControl = new FormControl(null, [Validators.required]);
+  cliente: FormControl = new FormControl(null, [Validators.required]);
+
+  constructor(private router: Router, private chamadoService: ChamadoService) {}
+
+  ngOnInit(): void { }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid && this.status.valid && this.titulo.valid && this.descricao.valid && this.tecnico.valid && this.cliente.valid;
+  }
+
+  create(): void {
+    this.chamadoService.create(this.tecnico).subscribe(() => {
+      this.chamadoService.showMessage('Chamado Criado com Sucesso!', true);
+      this.router.navigate(['*/chamados']);
+    })
+  }
 }
