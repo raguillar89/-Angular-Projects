976bcd309dbf78f815b9463f29454dcaea6f9180
Renan Pinho <renan_aguillarpinho@hotmail.com>
2023-01-24 16:16:35 -0300

    Ajustes de Códigos

--- HelpDesk/frontend/src/app/components/templates/nav/nav.component.ts
+++ HelpDesk/frontend/src/app/components/templates/nav/nav.component.ts
@@ -11,7 +11,7 @@ export class NavComponent {
   constructor(private router: Router, private signupService: SignupService){ }
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/read/1'])
+    this.router.navigate(['*'])
   }
 
   logout() {    
